80b15972e41e2a9520505547db1632591f2513d1
Gowthami@098 <gowthamisegireddy@gmail.com>
2025-01-08 16:43:17 +0530

    Intial commit

---
+++ Basics.py
@@ -0,0 +1,14 @@
+#Scripting way for writting the code:
+print('Scripting way of Printing Hello world')
+
+#Procedural way of writting the code:
+def message():
+   print('Procedural way of writting Hello, world')
+message()
+
+#Object orientation way of writting the code:
+class Demo:
+    def instance_method(self):
+        print('Object oriented way of printing: Hello world')
+d1 = Demo()
+d1.instance_method()
---
+++ VariblesDatatypes/Overview.py
@@ -0,0 +1,3 @@
+#1.Python is Dynamiclally typed progrmming language
+#2.In Python while declaring variables wenot need to mention data types.
+#3.Data typed is m 
